e18b0fca49edd5f01c87b014c8e8bc0d8cdcb2aa
olimar ramilo <olimarramilo@yahoo.com>
2023-05-28 21:17:02 +0800

    error fix

--- program.c
+++ program.c
@@ -131,7 +131,7 @@ int main() {
         else if(pid == 0) { 
             char message[BUFFER_SIZE];
             read(pipe_fds[i][1], message,sizeof(message));
-            print("Child received file: %s\n", message);
+            printf("Child received file: %s\n", message);
             strcat(message, " - Reply from child!");
         
             write(pipe_fds[i][1], message,strlen(message) + 1);
