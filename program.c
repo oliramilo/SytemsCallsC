cf1f62c0aa190e3f64661a432899a0b986be19d2
olimar ramilo <olimarramilo@yahoo.com>
2023-05-28 22:38:10 +0800

    error fix

--- program.c
+++ program.c
@@ -163,10 +163,6 @@ int main() {
         }
     }
 
-    for (int i = 0; i < size; i++) {
-        printf("Child process with PID %d completed.\n", waitpid(-1, &status, 0));
-    }
-
     free_list(files_list, size);
     return 0;
 }
