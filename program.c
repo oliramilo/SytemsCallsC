fb448eb1762d025a50b48d704b1c40280ccb4f29
olimar ramilo <olimarramilo@yahoo.com>
2023-05-28 21:21:03 +0800

    error fix

--- program.c
+++ program.c
@@ -129,6 +129,7 @@ int main() {
 
         /**Child process**/
         else if(pid == 0) { 
+            close(pipe_fds[i][0]);
             char message[BUFFER_SIZE];
             read(pipe_fds[i][1], message,sizeof(message));
             printf("Child received file: %s\n", message);
